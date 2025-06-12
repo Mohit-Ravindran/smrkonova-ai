ab7607710ced636ddfd26e7b9e21a0fd00fa8bda
surya <ssuryareddy2277@gmail.com>
2025-06-12 22:06:20 +0530

    responsive added

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,4 +94,13 @@ function initContactForm() {
 }
 
 
+// Initialize Lenis
+const lenis = new Lenis();
 
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
