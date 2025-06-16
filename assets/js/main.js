a81078f9fbde23e701ee3467823eb55975a53bc2
surya <ssuryareddy2277@gmail.com>
2025-06-16 22:48:58 +0530

    ui fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,3 +139,12 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('mainHeader');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
